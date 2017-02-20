2c751bd29ceccd965c1e075e3d350e43935ba250
Juan Posadas <jposadasc@gmail.com>
2017-02-19 18:28:45 -0800

    Created hello world for react

--- src/entry.js
+++ src/entry.js
@@ -1,1 +1,10 @@
-console.log('Entry Pointsasdad');
+import React from 'react';
+import { render } from 'react-dom';
+
+class App extends React.Component {
+    render() {
+        return <h1>Hello, World!</h1>;
+    }
+};
+
+render(<App />, document.getElementById('app'));
